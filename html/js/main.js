a0f053e336753351c33eca76d0d4f90cb626bcf4
VasiliyP <vektornsk@yandex.ru>
2016-06-21 18:57:56 +0600

    index 2 day

--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,11 @@
-$(function() {
+setTimeout( function() {
+    
+/*--- tabs ---*/    
+    
+    $('ul.popular-parts-tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.popular-parts-tabs').find('div.popular-parts-tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
 
-
-
-});
+}, 1000);
